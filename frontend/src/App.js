dc0c2d5cbce5b2add7553e08dfa1db7b39afe1e6
Priyanka Chourey <priyanka.choure10@gmail.com>
2023-08-03 16:31:44 +0530

    initial commit for frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.wrapper}>
+      <h2>Hello Viewer</h2>
+    </div>
+  );
+}
+
+export default App;
